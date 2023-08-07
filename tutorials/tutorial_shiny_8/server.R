a51dc3d046f4b700604aaa9fc6a747fc34177fe8
Ariane Ducellier <ariane.ducellier.pro@gmail.com>
2023-08-07 11:36:55 -0700

    write R files for shiny tutorial 8

---
+++ tutorials/tutorial_shiny_8/server.R
@@ -0,0 +1,59 @@
+library(ggplot2)
+
+function(input, output){
+
+  data(iris)
+
+  thePlot <- reactive(
+    ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width)) +
+      geom_point()
+  )
+
+  theData <- reactive(
+    iris
+  )
+
+  output$plot <- renderPlot({
+    thePlot()
+  })
+
+  output$downloadPlot <- downloadHandler(
+    filename <- function(){
+      paste("Iris_plot", Sys.Date(), ".png", sep="")
+    },
+    content <- function(file){
+      png(file, width=980, height=400,
+          units="px", pointsize=12,
+          bg="white", res=NA)
+      iris.plot <- thePlot()
+      print(iris.plot)
+      dev.off()
+    },
+    contentType = "image/png"
+  )
+
+  output$downloadData <- downloadHandler(
+    filename = function(){
+      "iris.csv"
+    },
+    content <- function(file){
+      write.csv(theData(), file)
+    },
+    contentType = "text/csv"
+  )
+  
+  output$plot_clickedpoints <- renderTable({
+    res <- nearPoints(iris,
+                      input$plot_click,
+                      "Sepal.Length",
+                      "Sepal.Width")
+    if (nrow(res) == 0)
+      return()
+    res
+  })
+
+  output$plot_hoverinfo <- renderPrint({
+    cat("Hover (throttled):\n")
+    str(input$plot_hover)
+  })  
+}
---
+++ tutorials/tutorial_shiny_8/ui.R
@@ -0,0 +1,25 @@
+fluidPage(
+  titlePanel("Iris dataset"),
+  fluidRow(
+    column(width = 8,
+           tabPanel("Iris",
+                    plotOutput("plot",
+                               height=300,
+                               click = "plot_click",  
+                               hover = hoverOpts(id = "plot_hover",
+                                                 delayType = "throttle"),
+                               brush = brushOpts(id = "plot_brush")
+                    ),
+                    downloadButton("downloadPlot",
+                                   label = "Download plot"),
+                    downloadButton("downloadData",
+                                   label = "Download data")
+           ),
+           h4("Clicked points"),
+           tableOutput("plot_clickedpoints")
+    ),
+    column(width = 4,
+           verbatimTextOutput("plot_hoverinfo")
+    )
+  )
+)
