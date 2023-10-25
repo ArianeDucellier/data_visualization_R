de7bb3bcbd7ad2f0316c6db939c5b3f11a8eda02
ArianeDucellier <ariane.ducellier.pro@gmail.com>
2023-10-25 14:42:00 -0700

    Finished first tutorial on Shiny

---
+++ tutorials/tutorial_shiny_1_inclass/minimal_example/server.R
@@ -0,0 +1,11 @@
+function(input, output){
+  
+  output$textDisplay = renderText({
+    paste0("You said '",
+           input$comment,
+           "'. There are ",
+           nchar(input$comment),
+           " characters in this."
+    )
+  })
+}
